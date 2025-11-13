b9c1f9eb0c2c977142d75a71a3a030ca7a6f3b64
Vrushab Shreenidhi <65905866+VrushabS@users.noreply.github.com>
2025-11-12 23:50:27 -0500

    Add main method for car loan calculation
    The following code is the direct project that I worked on in Codecademy! 🦁 *Roar*

--- CarLoan.java
+++ CarLoan.java
@@ -1,2 +1,24 @@
 public class CarLoan {
+    public static void main(String[] args) {
+        // main method YIPPIE
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("Error! You must take out a valid car loan.");
+        } else if (downPayment > (carLoan + downPayment)) {
+
+        } else if (downPayment >= carLoan) {
+            System.out.println("The car can be paid in full.");
+        } else {
+            int remainingBalance = carLoan - downPayment;
+            int months = 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
+        }
+    }
 }
