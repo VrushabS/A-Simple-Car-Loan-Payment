171ab1bb23eb69f97c64d8e42f6fa38fc68bf3da
Vrushab Shreenidhi <65905866+VrushabS@users.noreply.github.com>
2025-11-12 23:57:21 -0500

    Fix interest calculation in car loan payment
    1. Removed unnecessary else if {  }  branch from branch
    2. On Line 19. I fixed the equation to:
    
     (monthlyBalance * interestRate)  /  100 
    
    from:
    
     monthlyBalance / 100

--- CarLoan.java
+++ CarLoan.java
@@ -8,15 +8,13 @@ public class CarLoan {
 
         if (loanLength <= 0 || interestRate <= 0) {
             System.out.println("Error! You must take out a valid car loan.");
-        } else if (downPayment > (carLoan + downPayment)) {
-
         } else if (downPayment >= carLoan) {
             System.out.println("The car can be paid in full.");
         } else {
             int remainingBalance = carLoan - downPayment;
             int months = 12;
             int monthlyBalance = remainingBalance / months;
-            int interest = monthlyBalance / 100;
+            int interest = (monthlyBalance * interestRate) / 100;
             int monthlyPayment = monthlyBalance + interest;
             System.out.println(monthlyPayment);
         }
